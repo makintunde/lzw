d2c5e461c0a529de721562fdd9da2b2d6dc84f2e
ma4413 <michael.akintunde13@imperial.ac.uk>
2015-10-26 16:02:20 +0000

    Remove useless macros

--- lzw.cpp
+++ lzw.cpp
@@ -1,20 +1,4 @@
 #include <bits/stdc++.h>
-#define _ ios_base::sync_with_stdio(0);cin.tie(0);
-#define Inf 2147483647
-#define LL long long
-#define N 1000000
-
-#define F(i, a) for( int i = (0); i < (a); i++ )
-#define Fs(i, sz) for( size_t i = 0; i < sz.size (); i++ )
-#define Fe(i, x) for(typeof (x.begin()) i = x.begin(); i != x.end (); i++)
-#define coutf(a, b, c) for( int i = a; i <= (b); i++ ) cout << c[i] << " "; cout << endl
-#define coutff(a, b, c, d) for( int i = a; i <= (b); i++ ) cout << c[i] << ", " << d[i] << " "; cout << endl
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(),(x).end()
-#define fi first
-#define se second
-#define SZ(x) ((int)(x).size())
 
 using namespace std;
 
