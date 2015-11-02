be9439f866481be6e6e54a5fc5294d31faeaaa87
ma4413 <michael.akintunde13@imperial.ac.uk>
2015-11-02 22:53:39 +0000

    Add decoding tests

--- src/lzw.cpp
+++ src/lzw.cpp
@@ -11,6 +11,16 @@ void print_encode_instructions() {
   cout << " - The n dictionary entries." << endl;
 }
 
+void print_decode_instructions() {
+  cout << "Enter:\n";
+  cout << " - m (Number of elements in the dictionary)\n";
+  cout << " - The entries of the dictionary." << endl;
+
+  cout << "Enter:\n";
+  cout << " - n (The number of elements in the code)\n";
+  cout << " - The n entries of the code." << endl;
+}
+
 void print_str_results(string name, vector<string> &a) {
   cout << "--------------------------------------" << endl;
   cout << name << ": ";
@@ -37,9 +47,6 @@ void print_results(string name, vector<int> &a) {
 vector<int> encode(string x, vector<string> d) {
   int n, m, k = 0, l = 1, p, i;
 
-  //print_encode_instructions();
-  //string x;
-  //cin >> x;
   string sub;
   vector<string> s;
   vector<int> c;
@@ -79,38 +86,14 @@ vector<int> encode(string x, vector<string> d) {
         c.push_back(distance(d.begin(), find(d.begin(), d.end(), w)));
     }
   }
-  //print_results("C", c);
-  //print_results("D", d);
   return c;
 }
 
-void decode() {
-  vector<string> d; // Dictionary.
-  vector<int> c; // Code.
-  int m;
-  cout << "Enter:\n";
-  cout << " - m (Number of elements in the dictionary)\n";
-  cout << " - The entries of the dictionary." << endl;
-  cin >> m;
-  for ( int i = 0; i < m; ++i ) {
-    string next;
-    cin >> next;
-    d.push_back( next );
-  }
-
-  cout << "Enter:\n";
-  cout << " - n (The number of elements in the code)\n";
-  cout << " - The n entries of the code." << endl;
-  int n;
-  string x;
-  cin >> n;
-  for (int i = 0; i < n; ++i ) {
-    int next;
-    cin >> next;
-    c.push_back( next );
-  }
-  cout << endl;
-  cout << "--------------------------------------" << endl;
+// Decode a code into its original string, given a dictionary and a code.
+string decode(vector<string> d, vector<int> c) {
+  int m = d.size();
+  int n = c.size();
+  string x = "";
   for ( int i = 0; i < n; ++i ) {
     x.append( d[c[i]] );
     string s = "";
@@ -121,24 +104,30 @@ void decode() {
     }
     d.push_back( s );
   }
-  cout << "X: " << x << endl;
-  // print_results("D", d);
+  return x;
 } 
 
 TEST_CASE( "Can correctly encode strings" ) {
-  vector<string> vec_1 = {"a", "b", "c", "d", "r"}; 
-  vector<string> vec_2 = {"i", "m", "p", "s"};
-  vector<string> vec_3 = {"a", "b"};
+  vector<string> d_1 = {"a", "b", "c", "d", "r"}; 
+  vector<string> d_2 = {"i", "m", "p", "s"};
+  vector<string> d_3 = {"a", "b"};
 
-  vector<int> exp_1 = {0, 1, 4, 0, 2, 0, 3, 5, 7};
-  vector<int> exp_2 = {1, 0, 3, 3, 5, 7, 2, 2, 0};
-  vector<int> exp_3 = {0, 1, 2, 4, 3};
-  vector<int> exp_4 = {0, 1, 0};
-  
-  REQUIRE( encode( "aba", vec_3 ) == exp_4);
-  REQUIRE( encode( "ababababa", vec_3 ) == exp_3);
-  REQUIRE( encode( "abracadabra", vec_1 ) == exp_1);
-  REQUIRE( encode( "mississippi", vec_2 ) == exp_2); 
+  vector<int> c_1 = {0, 1, 4, 0, 2, 0, 3, 5, 7};
+  vector<int> c_2 = {1, 0, 3, 3, 5, 7, 2, 2, 0};
+  vector<int> c_3 = {0, 1, 2, 4, 3};
+  vector<int> c_4 = {0, 1, 0};
+ 
+  // Encoding tests. 
+  REQUIRE( encode( "abracadabra", d_1 ) == c_1);
+  REQUIRE( encode( "mississippi", d_2 ) == c_2); 
+  REQUIRE( encode( "ababababa", d_3 ) == c_3);
+  REQUIRE( encode( "aba", d_3 ) == c_4);
+
+  // Decoding tests.
+  REQUIRE( decode( d_1, c_1 ) == "abracadabra" );
+  REQUIRE( decode( d_2, c_2 ) == "mississippi" );
+  REQUIRE( decode( d_3, c_3 ) == "ababababa" );
+  REQUIRE( decode( d_3, c_4 ) == "aba" );
 }
 
 // Implementation of LZW Encoding and Decoding.
