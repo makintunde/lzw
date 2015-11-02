5948bd8b8d7c0e2b3c9eed291ce94d3a57f3c270
ma4413 <michael.akintunde13@imperial.ac.uk>
2015-11-02 19:44:47 +0000

    Fix algorithm with all tests passing

--- src/lzw.cpp
+++ src/lzw.cpp
@@ -34,57 +34,49 @@ void print_results(string name, vector<int> &a) {
 // Encode a string using LZW given a corresponding dictionary.
 // TODO: Use an 'encoder' object with accessors for private code and 
 // updated dictionary fields.
-vector<int> encode(string x, vector<string> &d) {
+vector<int> encode(string x, vector<string> d) {
   int n, m, k = 0, l = 1, p, i;
 
   //print_encode_instructions();
   //string x;
   //cin >> x;
   string sub;
-  //vector<string> d;
   vector<string> s;
   vector<int> c;
   vector<string>::iterator it1, it2;
 
-  //cin >> m;
   m = d.size();
   n = x.size();
-
-  for ( i = 0; i < m; ++i ) { 
-    //string next;
-    //cin >> next;
-    s.push_back(d[i]);
-    //d.push_back(next);
-  }
 
   // Iterate through string x.
   string w = "";
   stringstream ss;
-  i = 0;
   for (auto c_it = x.begin(); c_it < x.end(); c_it++) {
-    cout << "k: " << *c_it << endl;
     char k = *c_it;
     string next = w + k;
-    cout << "next: " << next << endl;
     auto it = find(d.begin(), d.end(), next);
+
     if (it != d.end()) {
       w = next;
+      if (c_it == x.end() - 1)
+        c.push_back(distance(d.begin(), find(d.begin(), d.end(), w)));
     } else {
       if (w.size() > 0) {
-        cout << "w: " << w << endl;
         size_t p = distance(d.begin(), find(d.begin(), d.end(), w));
-        cout << "p: " << p << endl;
         c.push_back(p);
       }
       d.push_back(w + k);
-      print_results("c", c);
-      print_str_results("d", d);
+
+      // Ensure that string stream is cleared after each iteration.
       ss.clear();
       ss.str(string());
+
+      // Converts k char to string.
       ss << k;
       ss >> w;
-      cout << "new w: " << w << endl;
-      if (c_it == x.end()-1) c.push_back(distance(d.begin(), find(d.begin(), d.end(), w)));
+
+      if (c_it == x.end() - 1)
+        c.push_back(distance(d.begin(), find(d.begin(), d.end(), w)));
     }
   }
   //print_results("C", c);
@@ -141,16 +133,17 @@ TEST_CASE( "Can correctly encode strings" ) {
   vector<int> exp_1 = {0, 1, 4, 0, 2, 0, 3, 5, 7};
   vector<int> exp_2 = {1, 0, 3, 3, 5, 7, 2, 2, 0};
   vector<int> exp_3 = {0, 1, 2, 4, 3};
+  vector<int> exp_4 = {0, 1, 0};
   
-  REQUIRE( encode( "mississippi", vec_2 ) == exp_2); // <- Not passing?
-  REQUIRE( encode( "abracadabra", vec_1 ) == exp_1);
+  REQUIRE( encode( "aba", vec_3 ) == exp_4);
   REQUIRE( encode( "ababababa", vec_3 ) == exp_3);
+  REQUIRE( encode( "abracadabra", vec_1 ) == exp_1);
+  REQUIRE( encode( "mississippi", vec_2 ) == exp_2); 
 }
 
 // Implementation of LZW Encoding and Decoding.
 //
 // TODO: OOP version (to allow mocking?)
-// TODO: Sort out testing framework.
 /*
  * int main() {
   encode( string s, vector<string> &d );
